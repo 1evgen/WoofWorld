6d3086de7d313a5392584eb61d83fc0954edf83b
Evgenii <evgusa83@gmail.com>
2024-04-20 22:57:55 +0300

    set up dependancy

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`The server started on ${PORT}`);
+});
